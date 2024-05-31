be8320324f204b93439be2c27a32410e3b0fce4b
Lucas Casas <lucascasasp@icloud.com>
2024-05-31 14:05:37 -0600

    Cleaned up code

--- frontend/src/components/Globe/globe.tsx
+++ frontend/src/components/Globe/globe.tsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useRef } from 'react';
-import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import getStarfield from './src/getStarfield';
-import { getFresnelMat } from './src/getFresnelMat';
+import React, { useEffect, useRef } from "react";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import getStarfield from "./src/getStarfield";
+import { getFresnelMat } from "./src/getFresnelMat";
 
 const ThreeGlobe: React.FC = () => {
   const mountRef = useRef<HTMLDivElement>(null);
@@ -14,7 +14,7 @@ const ThreeGlobe: React.FC = () => {
     const camera = new THREE.PerspectiveCamera(70, w / h, 0.1, 1000);
     camera.position.z = 3;
 
-    const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true }, );
+    const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
     renderer.setClearColor(0x000000, 0);
     renderer.setSize(w, h);
     renderer.toneMapping = THREE.ACESFilmicToneMapping;
@@ -25,7 +25,7 @@ const ThreeGlobe: React.FC = () => {
     }
 
     const earthGroup = new THREE.Group();
-    earthGroup.rotation.z = -23.4 * Math.PI / 180;
+    earthGroup.rotation.z = (-23.4 * Math.PI) / 180;
     scene.add(earthGroup);
 
     const controls = new OrbitControls(camera, renderer.domElement);
@@ -35,9 +35,9 @@ const ThreeGlobe: React.FC = () => {
     const loader = new THREE.TextureLoader();
     const geometry = new THREE.IcosahedronGeometry(1, detail);
     const material = new THREE.MeshPhongMaterial({
-      map: loader.load('/textures/00_earthmap1k.jpg'),
-      specularMap: loader.load('/textures/02_earthspec1k.jpg'),
-      bumpMap: loader.load('/textures/01_earthbump1k.jpg'),
+      map: loader.load("/textures/00_earthmap1k.jpg"),
+      specularMap: loader.load("/textures/02_earthspec1k.jpg"),
+      bumpMap: loader.load("/textures/01_earthbump1k.jpg"),
       bumpScale: 0.04,
     });
 
@@ -45,18 +45,18 @@ const ThreeGlobe: React.FC = () => {
     earthGroup.add(earthMesh);
 
     const lightsMat = new THREE.MeshBasicMaterial({
-      map: loader.load('/textures/03_earthlights1k.jpg'),
+      map: loader.load("/textures/03_earthlights1k.jpg"),
       blending: THREE.AdditiveBlending,
     });
     const lightsMesh = new THREE.Mesh(geometry, lightsMat);
     earthGroup.add(lightsMesh);
 
     const cloudsMat = new THREE.MeshStandardMaterial({
-      map: loader.load('/textures/04_earthcloudmap.jpg'),
+      map: loader.load("/textures/04_earthcloudmap.jpg"),
       transparent: true,
       opacity: 0.3,
       blending: THREE.AdditiveBlending,
-      alphaMap: loader.load('/textures/05_earthcloudmaptrans.jpg'),
+      alphaMap: loader.load("/textures/05_earthcloudmaptrans.jpg"),
     });
     const cloudsMesh = new THREE.Mesh(geometry, cloudsMat);
     cloudsMesh.scale.setScalar(1.003);
@@ -75,49 +75,37 @@ const ThreeGlobe: React.FC = () => {
     scene.add(sunLight);
 
     const animate = () => {
-        requestAnimationFrame(animate);
-      
-        const rotationSpeed = 0.0002;
-      
-        earthMesh.rotation.y += rotationSpeed;
-        lightsMesh.rotation.y += rotationSpeed;
-        cloudsMesh.rotation.y += rotationSpeed * 1.15;
-        glowMesh.rotation.y += rotationSpeed;
-        stars.rotation.y -= rotationSpeed / 10;
-      
-        renderer.render(scene, camera);
-      };
-      
-      animate();
+      requestAnimationFrame(animate);
+
+      const rotationSpeed = 0.001;
+
+      earthMesh.rotation.y += rotationSpeed;
+      lightsMesh.rotation.y += rotationSpeed;
+      cloudsMesh.rotation.y += rotationSpeed * 1.65;
+      glowMesh.rotation.y += rotationSpeed;
+      stars.rotation.y -= rotationSpeed / 10;
+
+      renderer.render(scene, camera);
+    };
 
     animate();
 
     const handleWindowResize = () => {
-        const newWidth = mountRef.current?.clientWidth || window.innerWidth;
-        const newHeight = mountRef.current?.clientHeight || window.innerHeight;
-        camera.aspect = newWidth / newHeight;
-        camera.updateProjectionMatrix();
-        renderer.setSize(newWidth, newHeight);
-      };
-    window.addEventListener('resize', handleWindowResize, false);
+      const newWidth = mountRef.current?.clientWidth || window.innerWidth;
+      const newHeight = mountRef.current?.clientHeight || window.innerHeight;
+      camera.aspect = newWidth / newHeight;
+      camera.updateProjectionMatrix();
+      renderer.setSize(newWidth, newHeight);
+    };
+    window.addEventListener("resize", handleWindowResize, false);
 
     return () => {
-      window.removeEventListener('resize', handleWindowResize);
+      window.removeEventListener("resize", handleWindowResize);
       mountRef.current?.removeChild(renderer.domElement);
     };
   }, []);
 
-  return (
-    <div className="container mx-auto px-4 py-10">
-      <div className="bg-gradient-to-r from-blue-500 to-green-500 shadow-lg rounded-lg p-6">
-        <h2 className="text-3xl font-bold text-white mb-4 text-center">Explore Climate News by Country</h2>
-        <div className="flex justify-center items-center">
-            <div className="rounded" ref={mountRef} style={{ width: '100%', height: '100vh' }} />
-        </div>
-        <p className="text-white text-center mt-4">Click on a country to see the latest climate news.</p>
-      </div>
-    </div>
-  );
+  return <div ref={mountRef} className="background-globe" />;
 };
 
 export default ThreeGlobe;
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,14 +1,10 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import Header from '../components/header';
-import Article from '../components/article';
-import ThreeGlobe from '../components/Globe/globe';
-import SearchBar from '../components/searchBar';
-import Pagination from '../components/pagination'; // Import the Pagination component
-import dynamic from 'next/dynamic';
-
-// Dynamically import the ThreeScene component
-const ThreeScene = dynamic(() => import('../components/three'), { ssr: false });
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Header from "../components/header";
+import Article from "../components/article";
+import ThreeGlobe from "../components/Globe/globe";
+import SearchBar from "../components/searchBar";
+import Pagination from "../components/pagination";
 
 interface Article {
   title: string;
@@ -25,13 +21,15 @@ const Home: React.FC = () => {
   const [totalPages, setTotalPages] = useState(1);
 
   const fetchArticles = async () => {
-    const response = await axios.get('http://127.0.0.1:5000/articles');
+    const response = await axios.get("http://127.0.0.1:5000/articles");
     setArticles(response.data);
     setTotalPages(Math.ceil(response.data.length / ITEMS_PER_PAGE));
   };
 
   const handleSearchResults = async (query: string) => {
-    const response = await axios.get(`http://127.0.0.1:5000/search?query=${query}`);
+    const response = await axios.get(
+      `http://127.0.0.1:5000/search?query=${query}`
+    );
     const searchResults = response.data.map((result: any) => result._source);
     setArticles(searchResults);
     setCurrentPage(1); // Reset to the first page on new search results
@@ -43,7 +41,10 @@ const Home: React.FC = () => {
   }, []);
 
   const startIndex = (currentPage - 1) * ITEMS_PER_PAGE;
-  const currentArticles = articles.slice(startIndex, startIndex + ITEMS_PER_PAGE);
+  const currentArticles = articles.slice(
+    startIndex,
+    startIndex + ITEMS_PER_PAGE
+  );
 
   const handlePageChange = (page: number) => {
     setCurrentPage(page);
