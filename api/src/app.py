7d4e5f6c14660e77992be32a734c760e335badf4
Lucas Casas <lucascasasp@icloud.com>
2024-08-13 16:56:19 -0400

    Added bulk processing

--- api/src/app.py
+++ api/src/app.py
@@ -154,7 +154,7 @@ def fetch_and_index_events():
         for event in processed_events:
             uri = event["uri"]
             if uri not in urls_seen:
-                urls_seen.add(uri)
+                uris_seen.add(uri)
                 action = {
                     "_index": "events",
                     "_id": event['uri'],
